4acc3868d9e9f697fedca710f9dad6d97d7f8ff0
Robert Mock <robertjamesmock@gmail.com>
2016-12-10 01:16:52 -0800

    init commit for room idea

---
+++ New Unity Project/Assets/bodyscript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class bodyscript : MonoBehaviour {
+	public float hSensitivity=1;
+	Rigidbody myBody;
+
+	void Awake() { myBody = GetComponent<Rigidbody>(); }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(new Vector3(0,Input.mousePosition.x*hSensitivity,0));
+
+		myBody.AddRelativeForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+		
+	}
+}
---
+++ New Unity Project/Assets/head.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class head : MonoBehaviour {
+	public float vSensitivity = 1;
+
+	float vangle;
+	float lastMouseY;
+	// Use this for initialization
+	void Start () {
+		lastMouseY = Input.mousePosition.y;
+		vangle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseY = Input.mousePosition.y;
+		vangle -= mouseY - lastMouseY;
+		lastMouseY = mouseY;
+        vangle = Mathf.Clamp(vangle, -90, 90);
+		transform.localRotation = Quaternion.Euler(new Vector3( vangle, 0));
+	}
+}
