c8ad22aa9daa31471d31e2d6048cebaef67d22fd
Robert Mock <robertjamesmock@gmail.com>
2016-12-11 12:49:07 -0800

    commiting cuz battery

---
+++ New Unity Project/Assets/negativeMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class negativeMover : MonoBehaviour
+{
+    public Transform zeroTarget;
+    Transform toCopy;
+    // Use this for initialization
+    void Start()
+    {
+        toCopy = FindObjectOfType<FirstPositioner>().GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = zeroTarget.rotation * Quaternion.Inverse(toCopy.rotation);
+        transform.position = zeroTarget.position - toCopy.position * transform.localScale.magnitude;
+    }
+}
---
+++ New Unity Project/Assets/relativeChild.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class relativeChild : MonoBehaviour {
+    Transform toCopy;
+	// Use this for initialization
+	void Start () {
+        toCopy = FindObjectOfType<FirstPositioner>().GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = toCopy.position;
+        transform.rotation = toCopy.rotation;
+	}
+}
